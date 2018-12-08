eea251d32d7e10bdcb20d9760a69ba4baad806c6
lYesterdaYl <bgmds@126.com>
2018-12-08 09:29:24 -0800

    feat
    updated healper functions(html formator and httpPost)

---
+++ include/function.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zhiyuan Du
+ * Date: 12/7/2018
+ * Time: 12:53 PM
+ */
+
+
+function html_to_obj($html) {
+    $dom = new DOMDocument();
+    $dom->loadHTML($html);
+    return element_to_obj($dom->documentElement);
+}
+
+function element_to_obj($element) {
+    $obj = array( "tag" => $element->tagName );
+    foreach ($element->attributes as $attribute) {
+        $obj[$attribute->name] = $attribute->value;
+    }
+    foreach ($element->childNodes as $subElement) {
+        if ($subElement->nodeType == XML_TEXT_NODE) {
+            $obj["html"] = $subElement->wholeText;
+        }
+        else {
+            $obj["children"][] = element_to_obj($subElement);
+        }
+    }
+    return $obj;
+}
+
+function httpPost($url, $param, $post_file=false){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE) {
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+    }
+    if (is_string($param) || $post_file) {
+        $strPOST = $param;
+    } else {
+        $aPOST = array();
+        foreach($param as $key=>$val){
+            $aPOST[] = $key."=".urlencode($val);
+        }
+        $strPOST =  join("&", $aPOST);
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($oCurl, CURLOPT_POST,true);
+    curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200) {
+        return $sContent;
+    } else {
+        return false;
+    }
+}
